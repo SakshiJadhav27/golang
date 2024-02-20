055b605517a9ff5c9b31661452a06bb48236d163
Sakshi jadhav <124150607+SakshiJadhav27@users.noreply.github.com>
2024-02-20 16:59:08 +0530

    commit model

---
+++ http-api/model/model.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"errors"
+	"time"
+)
+
+//go:generate mockgen -destination=../mocks/mock_repository.go -package=mocks github.com/shijuvar/gokit/examples/http-api/model Repository
+
+var ErrNotFound = errors.New("No records found")
+var ErrNoteExists = errors.New("Note title exists")
+
+type Note struct {
+	NoteID      string    `json:"noteid,omitempty"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedOn   time.Time `json:"createdon,omitempty"`
+}
+
+// CRUD interface
+type Repository interface {
+	Create(Note) error
+	Update(string, Note) error
+	Delete(string) error
+	GetById(string) (Note, error)
+	GetAll() ([]Note, error)
+}
